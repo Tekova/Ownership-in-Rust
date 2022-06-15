4683e28bd245e24b98f012f353699a0240493f43
unknown <me.quicksilver2019@gmail.com>
2022-06-15 12:32:22 +0530

    Target added

---
+++ target/debug/deps/ownership_in_rust.d
@@ -0,0 +1,5 @@
+D:\Rust Programs\ownership_in_rust\target\debug\deps\ownership_in_rust.exe: src\main.rs
+
+D:\Rust Programs\ownership_in_rust\target\debug\deps\ownership_in_rust.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/ownership_in_rust.d
@@ -0,0 +1,1 @@
+D:\Rust\ Programs\ownership_in_rust\target\debug\ownership_in_rust.exe: D:\Rust\ Programs\ownership_in_rust\src\main.rs
